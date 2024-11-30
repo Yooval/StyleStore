4189d4395ab8ebf1768d7facfbc045bd6b4d39c1
Yooval <116361337+Yooval@users.noreply.github.com>
2024-11-30 15:14:57 +0200

    Delete mystore directory

--- mystore/src/com/itbulls/learnit/javacore/oop/exam/onlineshop/enteties/UserRole.java
+++
@@ -1,9 +0,0 @@
-package com.itbulls.learnit.javacore.oop.exam.onlineshop.enteties;
-
-// This file is an enum defining user roles, such as CUSTOMER, ADMIN, and SELLER.
-
-public enum UserRole {
-    CUSTOMER,
-    ADMIN,
-    SELLER 
-}
